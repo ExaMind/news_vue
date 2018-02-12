a2e1ed2af7da9c3e1c649229de261e6d44efb521
Sudarshan <srn1@outlook.com>
2018-02-11 21:16:05 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">News</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Get News</a></li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <main>
+    <router-view/>
+  </main>
+
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .nav-wrapper {
+    background-color: #00ced1;
+    font-size: 14px;
+    font-weight: bold;
+  }
+</style>
